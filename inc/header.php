f4b67bc0e4d119383f54a5351f94a9d3388bbc05
jc mairot <j.mairot@codeur.online>
2020-11-02 17:27:14 +0100

    commit fin de journée modification footer/header/init

--- inc/header.php
+++ inc/header.php
@@ -3,13 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title | ....</title>
+    <link rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" 
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="../asset/css/distripizz.css">
+    <title>Distripizz</title>
 </head>
 <body>
+    <header>
+      <nav class=""></nav>  
+    </header>
     
-    <nav class="">
-      
-    </nav>
 
 
 
--- inc/init.php
+++ inc/init.php
@@ -5,7 +5,7 @@ session_start();
 
 // CONNEXION A LA BDD 
 //
-$pdo = new PDO('mysql:host=localhost;dbname=', 'root', '', array(
+$pdo = new PDO('mysql:host=localhost;dbname=collaborateurs', 'root', '', array(
 	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,	
 	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
