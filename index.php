3517d10cad0dbf7d0027504e442cdd71f9e018cc
jc mairot <j.mairot@codeur.online>
2020-11-02 13:43:53 +0100

    Le premier commit pour la creation du projet Distripizz

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//fait appel à init.php dans dossier inc (inclusion)
+require_once('inc/init.php');
+
+//code php
+
+
+require_once('inc/header.php');
+?>
+
+
+
+//php et html pour le corps de la page
+
+
+
+
+<?php 
+include('inc/footer.php');
+?>
