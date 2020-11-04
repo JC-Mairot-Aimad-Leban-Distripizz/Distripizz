fe278ed67c1e6e2c372dc675d8f0084fa181215a
jc mairot <j.mairot@codeur.online>
2020-11-04 17:31:20 +0100

    commit suppr carousel doc

--- documentation.php
+++ documentation.php
@@ -18,6 +18,37 @@ require_once('inc/header.php');
     </div> 
 <?php endforeach ?>
 
+
+<div>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+
+    <div class="carousel-inner">
+
+        <div class="carousel-item ">
+        <img src="img/01.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item active">
+        <img src="img/02.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/03.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="img/04.jpg" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    </div>
+
+</div>
+
 <?php 
 include('inc/footer.php');
 ?>
