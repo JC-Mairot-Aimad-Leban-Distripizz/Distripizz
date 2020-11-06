66c95ac293c82cbde3597a66313cf30328b5eca3
jc mairot <j.mairot@codeur.online>
2020-11-06 14:23:16 +0100

    ajout deco php et dans index un plan en comment

--- index.php
+++ index.php
@@ -7,10 +7,19 @@ require_once('inc/fonctions.php');
 
 require_once('inc/header.php');
 ?>
+<!-- div générale pour le contenu de la onepage -->
+<div class="container">
+<!-- carousel bootstrap -->
+<!-- block h1 + p qui somme nous -->
+<!-- une image bouteille + pizza possibilité hover annimé-->
+<!-- 1div>2div>h2+p -->
+<!-- 1 img team -->
+<!-- 1div>2div>h2+p -->
+<!-- 1div>2div>h2+p+img -->
+<!-- formulaire contactez nous avec une ancre de contact du header -->
+<!-- img  -->
+</div>
 
-<div class="test2"></div>
-
-//php et html pour le corps de la page
 
 
 
